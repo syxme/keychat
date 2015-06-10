9ac9fb09d7b4ac2fa888c507e516dda481c457bc
Dmitry Shebaldin <dima.shebaldin@progforce.com>
2015-06-10 17:55:22 +0300

    more new

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-
+var md = require("node-markdown").Markdown;
 
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
@@ -14,25 +14,62 @@ var users = [{
 	id:id
 }];
 
-
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * (16 - 5) + 5)];
+    }
+    return color;
+}
+var a = 0;
 io.on('connection', function(client){
 	client.user = {};
   console.log('Connected');
+    
+    client.user.room = 'freesponds';
+    client.user.keypass = '';
+    client.user.name = client.user.name===undefined? 'Guest':client.user.name;
+    client.user.color = getRandomColor();
+    client.join(client.user.room);
 
-  io.emit('event', {name:"BOT",msg:'В Час зашел новый человек под номером '+client.id});
+
+  client.broadcast.to(client.user.room).emit('event', {name:"BOT",msg:'В комнату зашел новый человек под номером '+client.id});
+
   client.on('set_name',function(name){
-  	var storename = client.user.name;
     client.user.name = name;
-  	io.emit('chat message', {name:'BOT',msg:'Пользователь '+storename+" переименовался на "+client.user.name});
+  	client.broadcast.to(client.user.room).emit('chat message', {name:'BOT',msg:'Пользователь '+client.user.name+" переименовался на "+client.user.name});
   });
-  client.on('disconnect', function(){
-    console.log('Disconnected');
-  });
+
+  client.on('disconnect', function(){console.log('Disconnected');});
 
   client.on('chat message', function(msg){
-    client.user.name = client.user.name===undefined? 'Guest':client.user.name;
-    io.emit('chat message', {name:client.user.name,msg:msg.replace(/(?:\r\n|\r|\n)/g, '<br />')});
+    if (msg.substr(0,3) =='//-'){
+      client.leave(client.user.room);
+      client.user.room = msg.substr(4,msg.length);
+      console.log(client.user.room); 
+      client.join(client.user.room); 
+      client.emit('event', {name:"BOT",msg:'В комнате, можно писать и не очковать))'});
+      client.broadcast.to(client.user.room).emit('event', {name:"BOT",msg:'В комнате под номером '+client.user.room+' новый чел ))'});
+    }else{
+
+    io.sockets.in(client.user.room).emit('chat message', {
+      color :client.user.color,
+      name  :client.user.name,
+      msg   :msg.replace(/(?:\r\n|\r|\n)/g, '<br />')
+    });
+
+    }
+
   });
+
+  client.on('keypress', function(msg){
+      client.broadcast.to(client.user.room).emit('keypress', {name:client.user.name});
+  });
+
+
+
+
 });
 
 
--- public/js/app.js
+++ public/js/app.js
@@ -6,21 +6,29 @@ function getRandom(min, max)
 
 
 $(document).ready( function(){
+	var messagem = new Audio("/media/msg.wav");
+
 	var socket = io();
 	var optionShow = true;
+
 	$('.rename').click(function(){
 		socket.emit('set_name',$('#chat_name').val())
 	});
-
+	$('#m').keypress(function(){
+		socket.emit('keypress','x');
+	});
 	$('#m').keydown(function() {
     if (event.keyCode == 13&&!event.shiftKey) {
-       	sendMessage(-1,$('#m').val());
-       	$('#m').val('');
+    	var msgs = $('#m').val();
+    	if (msgs!=''){
+       		sendMessage(-1,msgs);
+       		$('#m').val('');
+       	}
         return false;
      }
 	});
 	$('.options-btn').click(function(){
-		$('.options').css('bottom',optionShow? '100px':"22px");
+		$('.options').css('bottom',optionShow? '100px':"7px");
 		optionShow = !optionShow;
 	});
 	$('form span').click(function(){
@@ -35,17 +43,26 @@ $(document).ready( function(){
 		}
 	}
 
-
 	function message(msg){
-		console.log(msg)
-		msg.msg = msg.msg;
-		$('#messages').append('<div><b class="green">'+msg.name+': </b>'+msg.msg+'</div>');
+		messagem.play();
+		var time = new Date();
+		time = time.getHours()+':'+time.getMinutes()+':'+time.getSeconds();
+		$('#messages').append('<div class="ms"><b style="color:'+msg.color+'">'+msg.name+': </b>'+msg.msg+'<div class="timed">'+time+'</div></div>');
        	scroll(0,document.body.scrollHeight);
 	}
-
+	var time_out_id = 0;
 
 
 	//SOCK
+
+
+	socket.on('keypress', function(msg){
+		clearTimeout(time_out_id);
+		$('.keypress').text(msg.name+' набирает сообщение').css('display','block');
+		time_out_id = setTimeout("$('.keypress').text('').css('display','none')",500);
+	});
+
+
 	socket.on('chat message', function(msg){
 		message(msg)
 	});
